e92b07f29a25de44be8c0510aa5037fe4edd0a67
Ajinkya Jayawant <ajinkyajayawant1@gmail.com>
2020-07-05 00:49:26 -0700

    Experiments with type declarations.

--- haskell/basic_usage.hs
+++ haskell/basic_usage.hs
@@ -1,7 +1,51 @@
+-- Practising from the Haskell wiki book
+
 squareit x = x * x
 
 addthree a b c = a + b + c
 
 mymodulo x
-	 | x<0 = -x
-	 | otherwise = x
+  | x<0 = -x
+  | otherwise = x
+
+num_real_solns a b c
+  | det >0 = 2
+  | det ==0 = 1
+  | otherwise = 0
+    where
+      det = b^2 - 4*a*c
+
+area_triangle_heron a b c = result
+  where
+    result = sqrt (s*(s-a)*(s-b)*(s-c))
+    s = (addthree a b c)/2
+
+-- No specific indentation is needed for if else statements?? Textbook shows a different indentation
+my_signum a =
+  if a<0
+  then -1
+  else if a>0
+  then 1
+  else 0
+
+pieces :: Int -> Int
+pieces 1 = 10
+pieces 2 = 11
+pieces _ = 0
+
+-- Composition seems to work with only single input functions
+sq_signum x = (squareit.my_signum) x
+
+-- Seeing the type of a function which returns three variables
+myconcat a b = (a, b)
+
+-- Tuple addition with constraints
+tupConstAdd :: (Num a) => (a, a) -> a
+tupConstAdd (a, b) = a + b
+
+-- Trying to not explicitly define and see what happens
+tupAdd (a, b) = a + b
+
+-- Trying generic type definitions
+tup2lst :: (a, a) -> [a]
+tup2lst (x, y) = [x, y]
---
+++ haskell/cmline.hs
@@ -0,0 +1,17 @@
+:load basic_usage
+
+:t myconcat
+-- myconcat :: t -> t1 -> (t, t1)
+-- So looks like haskell functions return only one value. To read the -> notation basically only the last value is the value retuned by the function. Since in FP functions are ideally one argument one output, -> aptly represents the currying.
+
+tupConstAdd (2, 3)
+tupConstAdd ("aj", "dj")
+
+:t tupAdd
+-- This returns the exact same thing as :t tupConstAdd, haskell does type inference here I think
+-- Something to note is that Haskell does not allow me to define the type of tupAdd as tupAdd :: (a,a) -> a. I think that is because the type cannot be a generic one a.
+
+tup2lst (2, 3)
+tup2lst ("aj", "dj")
+tup2lst (2,"zj")
+-- The last command gives back an error as expected

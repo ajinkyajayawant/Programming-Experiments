9f4643573f313acad9ab8427ef555d3167bdf0fe
Ajinkya Jayawant <ajinkyajayawant1@gmail.com>
2021-01-12 23:39:18 -0800

    First unrun attempt.

--- haskell/HaskellWikibook/date_pg111.hs
+++ haskell/HaskellWikibook/date_pg111.hs
@@ -16,3 +16,26 @@ showAnniversary :: (Birthday name year month day) =
 
 showAnniversary :: (Wedding name1 name2 year month day) =
   name ++ " married " ++ name2 ++ " on " ++ showDate year month day
+
+-- My solution
+data Date = Ymd Int Int Int
+
+-- access
+myshowDate :: Date -> String
+myshowDate (Ymd y m d) = show y ++ "-" ++ show m ++ "-" ++ show d
+
+data myAnniversary = myBirthday String Date
+                   | myWedding String String Date
+
+myshowAnniversary :: myAnniversary -> String
+
+myshowAnniversary :: (myBirthday name fixedDate) =
+  name ++ " born " ++ myshowDate fixedDate
+
+myshowAnniversary :: (myWedding name1 name2 fixedDate) =
+  name ++ " married " ++ name2 ++ " on " ++ myshowDate fixedDate
+
+-- Executing and checking
+let date1 = Ymd 1970 10 24
+let Janniversary = myBirthday "John" date1
+myshowAnniversay Janniversary

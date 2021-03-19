33f049f08d07f8471c72620b9e1c6cc0b0d6b51f
Ajinkya Jayawant <ajinkyajayawant1@gmail.com>
2021-03-18 23:54:45 -0700

    Experiments on yielding tuples twice

--- python/yield_experiments.py
+++ python/yield_experiments.py
@@ -5,10 +5,20 @@ def generator_100():
     for i in np.linspace(1,100,100):
         yield i
 
-       
+
+def tuple_generator_100():
+    for i in np.linspace(1,100,100):
+        yield i, i+1
+
+
 def adder_5(our_ints):
     for i5 in our_ints:
         yield i5+5
+
+
+def tuple_adder_5():
+    for i5, i12 in tuple_generator_100():
+        yield i5+5, i12+12
 
 
 def yield_caller():
@@ -17,3 +27,7 @@ def yield_caller():
 
     for y in it_105:
         print(y, end=" ")
+
+def yield_tuple_caller():
+    for y1, y2 in tuple_adder_5():
+        print("({0}, {1})".format(y1, y2), end=" ")

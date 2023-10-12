239b2d5b62ec3a72899528425bc43dc421fd97c7
Ajinkya Jayawant <ajinkyajayawant1@gmail.com>
2023-10-12 09:27:25 +0530

    Hackerrank question soln for list filtering based on index

--- haskell/hackerrank/filter_positions_list.hs
+++ haskell/hackerrank/filter_positions_list.hs
@@ -1,16 +1,9 @@
 f :: [Int] -> [Int]
 f lst = new_lst where
-    new_lst = []
-
-f_cond :: Int -> [Int] -> [Int]
-f_cond flag arr = new_lst where
-    new_arr = if arr == []
-    then []
-    else head_el = head arr
-        rest_arr = tail arr
-
+    zip_lst = zip [0..] lst
+    new_lst = [x | (i,x) <- zip_lst, mod i 2 == 1]
 
 -- This part deals with the Input and Output and can be used as it is. Do not modify it.
 main = do
-    inputdata <- getContents
-    mapM_ (putStrLn. show). f. map read. lines $ inputdata
+	inputdata <- getContents
+	mapM_ (putStrLn. show). f. map read. lines $ inputdata
